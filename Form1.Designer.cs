34e1cedd3e4f39477291b5b737f28b0a22374151
sadbarash <49390474+sadbarash@users.noreply.github.com>
2021-04-22 23:54:56 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Kółko_i_krzyżyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Kto = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.wynikO = new System.Windows.Forms.Label();
+            this.wynikX = new System.Windows.Forms.Label();
+            this.RestartB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(146, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(202, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(258, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(146, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(202, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(258, 142);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(146, 198);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(202, 198);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(258, 198);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(146, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "teraz gra:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Kto
+            // 
+            this.Kto.AutoSize = true;
+            this.Kto.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Kto.Location = new System.Drawing.Point(236, 260);
+            this.Kto.Name = "Kto";
+            this.Kto.Size = new System.Drawing.Size(26, 25);
+            this.Kto.TabIndex = 10;
+            this.Kto.Text = "O";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(350, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 37);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "wynik:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(365, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 37);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "O:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(368, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 37);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "X:";
+            // 
+            // wynikO
+            // 
+            this.wynikO.AutoSize = true;
+            this.wynikO.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.wynikO.Location = new System.Drawing.Point(405, 152);
+            this.wynikO.Name = "wynikO";
+            this.wynikO.Size = new System.Drawing.Size(23, 25);
+            this.wynikO.TabIndex = 14;
+            this.wynikO.Text = "0";
+            this.wynikO.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // wynikX
+            // 
+            this.wynikX.AutoSize = true;
+            this.wynikX.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.wynikX.Location = new System.Drawing.Point(404, 208);
+            this.wynikX.Name = "wynikX";
+            this.wynikX.Size = new System.Drawing.Size(23, 25);
+            this.wynikX.TabIndex = 15;
+            this.wynikX.Text = "0";
+            // 
+            // RestartB
+            // 
+            this.RestartB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.RestartB.Location = new System.Drawing.Point(146, 323);
+            this.RestartB.Name = "RestartB";
+            this.RestartB.Size = new System.Drawing.Size(162, 42);
+            this.RestartB.TabIndex = 16;
+            this.RestartB.Text = "RESTART";
+            this.RestartB.UseVisualStyleBackColor = true;
+            this.RestartB.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.RestartB);
+            this.Controls.Add(this.wynikX);
+            this.Controls.Add(this.wynikO);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Kto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kółko i krzyżyk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Kto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label wynikO;
+        private System.Windows.Forms.Label wynikX;
+        private System.Windows.Forms.Button RestartB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kółko_i_krzyżyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool gracz1 = true;
+        int ruch = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Restart();
+            wynikO.Text = "0";
+            wynikX.Text = "0";
+            Kto.Text = "O";
+            gracz1 = true;
+        }
+
+        private void Restart()
+        {
+            ruch = 0;
+            Button[] all = new Button[9];
+            all[0] = button1;
+            all[1] = button2;
+            all[2] = button3;
+            all[3] = button4;
+            all[4] = button5;
+            all[5] = button6;
+            all[6] = button7;
+            all[7] = button8;
+            all[8] = button9;
+            foreach(Button b in all)
+            {
+                b.Enabled = true;
+                b.Text = "";
+            }
+        }
+        private void Sprawdz()
+        {
+            if (button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
+            {
+                Wygrana();
+            }
+            else if (button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
+            {
+                Wygrana();
+            }
+            else if (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text) 
+            {
+                Wygrana();
+            }
+            else if (button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text) 
+            {
+                Wygrana();
+            }
+            else if (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text) 
+            {
+                Wygrana();
+            }
+            else if (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text) 
+            {
+                Wygrana();
+            }
+            else if (button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text) 
+            {
+                Wygrana();
+            }
+            else if (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text) 
+            {
+                Wygrana();
+            }
+            else if(ruch == 9)
+            {
+                MessageBox.Show("Remis", "Koniec gry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Restart();
+            }
+        }
+
+        private void Wygrana()
+        {
+            MessageBox.Show("Wygrywa gracz: " + (gracz1 ? "O" : "X"), "Wygrana", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if (gracz1)
+                wynikO.Text = ((int.Parse(wynikO.Text)) + 1).ToString();
+            else
+                wynikX.Text = ((int.Parse(wynikX.Text)) + 1).ToString();
+            Restart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if(ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+ 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            ((Button)sender).Text = gracz1 ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch >= 5)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            Kto.Text = gracz1 ? "O" : "X";
+        }
+    }
+}
